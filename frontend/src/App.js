25ffbf1f33f81122ed4da7cd7c93e36620947512
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2024-01-13 14:43:11 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
+  const [data, setData]=useState([]);
+
+  useEffect(()=>{
+
+  },[])
   return (
     <div className="App">
+      <div className="ml-[30%]">
+      <table className="border">
+            <tr className="border">
+                <th className="border p-4">Company</th>
+                <th className="border p-4">Contact</th>
+                <th className="border p-1">Country</th>
+            </tr>
+            <tr>
+                <th className="border p-4">Alfreds Futterkiste</th>
+                <th className="border p-4">Maria Anders</th>
+                <th className="border p-4">Germany</th>
+            </tr>
+      </table>
+    </div>
       
     </div>
   );
